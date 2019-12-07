9041cf4819658639a271558b442bbfc5fd218848
Celine <celine.dannappe@gmail.com>
2019-12-07 17:52:05 +0100

    targets

---
+++ Limonadier/Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by celine dann on 07/12/2019.
+//  Copyright © 2019 celine dann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
